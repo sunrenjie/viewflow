878322db84ad980ab2b060776ab4c0728f2c06be
Renjie Sun <sunrenjie6@gmail.com>
2017-08-28 15:06:07 +0800

    More correct MySQL commands in init.sh

--- demo/init.sh
+++ demo/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mysql -e 'DROP DATABASE viewflow;'
+mysql -e 'DROP DATABASE IF EXISTS viewflow;'
 mysql -e 'CREATE DATABASE viewflow DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;'
 python3 ../manage.py migrate
 python3 ../manage.py loaddata helloworld/fixtures/helloworld/default_data.json
